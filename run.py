24f3e48b037f3e25008f94b5ad627f175a64a14e
Samantha Atkinson <sammodeste@yahoo.com>
2024-09-28 19:20:50 +0100

    refactored colour functions
    Removed the colour functions. Made a list of all the colours to insert as a value to the function print_colour.

--- run.py
+++ run.py
@@ -5,84 +5,51 @@ from pyfiglet import Figlet
 dance_title = Figlet(font="georgia11", justify="center")
 game_title = Figlet(font="doom", justify="center")
 
+# colours for the print colour function
+colours = ["light_grey", "light_red", "light_green",
+           "light_blue", "light_magenta", "light_cyan", "light_yellow"]
 
-# Colour Functions
 
-
-def print_color_light_magenta(text):
+# Colour Function
+def print_colour(text, color):
     """
-    Colours text a light magenta
+    Prints text in colours
     """
-    return cprint(text, "light_magenta", attrs=["bold"])
-
-
-def print_color_light_blue(text):
-    """
-    Colours text a light blue
-    """
-    return cprint(text, "light_blue")
-
-
-def print_color_light_red(text):
-    """
-    Colours text a light red
-    """
-    return cprint(text, "light_red", attrs=["bold"])
-
-
-def print_color_light_yellow(text):
-    """
-    Colours text a light yellow
-    """
-    return cprint(text, "light_yellow", attrs=["bold"])
-
-
-def print_color_light_grey(text):
-    """
-    Colours text a light grey
-    """
-    return cprint(text, "light_grey", attrs=["bold"])
-
-
-def print_color_light_green(text):
-    """
-    Colours text a light green
-    """
-    return cprint(text, "light_green", attrs=["bold"])
+    return cprint(text, color, attrs=["bold"])
 
 
 def game_title_intro_text():
     """
     Game Title and game intro text
     """
-    print_color_light_blue(dance_title.renderText("En Pointe"))
-    print_color_light_magenta(game_title.renderText("Dance Game"))
-    print_color_light_magenta("En Pointe Dance Academy Adventure Game")
-    print_color_light_blue("Step into the world of dance.")
-    print_color_light_blue(
-        "Follow the journey of one of three unique dancers,")
-    print_color_light_blue("in their final year of En Pointe Dance Academy.")
-    print_color_light_magenta(
-        "This is a story driven adventure, where every choice matters!")
+    print_colour(dance_title.renderText("En Pointe"), colours[3])
+    print_colour(game_title.renderText("Dance Game"), colours[4])
+    print_colour("En Pointe Dance Academy Adventure Game", colours[4])
+    # print_color_light_blue("Step into the world of dance.")
+    # print_color_light_blue(
+    #     "Follow the journey of one of three unique dancers,")
+    # print_color_light_blue("in their final year of En Pointe Dance Academy.")
+    # print_color_light_magenta(
+    #     "This is a story driven adventure, where every choice matters!")
     while True:
         try:
-            print_color_light_grey("Ready to start your adventure?")
-            print_color_light_magenta(
-                "When the music starts, a dancer knows when to start dancing,")
-            print_color_light_magenta(
-                "because the teacher shouts 4 numbers to the beat of the music.")
+            print_colour("Ready to start your adventure?", colours[0])
+            print_colour(
+                "When the music starts, a dancer knows when to start dancing,", colours[4])
+            print_colour(
+                "because the teacher shouts 4 numbers to the beat of the music.", colours[4])
             start_game = input(colored("So Type: 5, 6, 7, 8\n",
                                "light_grey", attrs=["bold"]))
             if start_game != "5,6,7,8":
                 raise ValueError("Error: Did you type the right numbers?")
 
-            print_color_light_green(
-                "Great, lets introduce you to our three dance characters.")
+            print_colour(
+                "Great, lets introduce you to the adventure.", colours[2])
             break
 
         except ValueError as e:
-            print_color_light_red(e)
-            print_color_light_yellow("Try again")
+            print_colour(e, colours[1])
+            print_colour("Try again", colours[2])
 
 
 game_title_intro_text()
