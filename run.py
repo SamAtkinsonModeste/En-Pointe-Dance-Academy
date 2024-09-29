d946400c9fc4bf72902a1f63e9c483c342bd0c3a
Samantha Atkinson <sammodeste@yahoo.com>
2024-09-29 01:38:57 +0100

    Trying to get correct space using \n

--- run.py
+++ run.py
@@ -80,10 +80,11 @@ def game_title_intro_text():
     """
     print_colour(georgia11_font.renderText("En Pointe"), colours[5])
     print_colour(doom_font.renderText("Dance Game"), colours[4])
-    print_colour("En Pointe Dance Academy Adventure Game", colours[4])
+    print_colour("En Pointe Dance Academy Adventure Game\n\n", colours[4])
     while True:
         try:
-            print_colour("Ready to step into the world of dance?", colours[0])
+            print_colour(
+                "Ready to step into the world of dance?\n", colours[0])
             print_colour(
                 "When the music starts,", colours[4])
             print_colour(
