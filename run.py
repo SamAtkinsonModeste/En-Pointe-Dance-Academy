6413faab111daffc09013149a52be1912cd7cd8d
Samantha Atkinson <sammodeste@yahoo.com>
2024-09-30 11:18:45 +0100

    created a reaction class and game_play function

--- run.py
+++ run.py
@@ -37,22 +37,56 @@ class Student:
 
 
 zoe = Student('Zoe', emoji.emojize(
-    "A rebellious :fire: triple-threat dancer."),
+    "A rebellious triple-threat dancer.:fire:"),
     'Torn between jazz and ballet,\n'
     'hiding her true dream\n'
     'to be a principal ballerina.')
-print_colour(zoe.description(), colours[3])
+# print_colour(zoe.description(), colours[3])
 
 sara = Student('Sara', emoji.emojize(
     "A determined ballet dancer,:flexed_biceps:"),
     'struggling with technique and self-confidence,\n'
     'trying to reconnect with the passion\n'
     'for dance she has lost with her confidence.')
-print_colour(sara.description(), colours[5])
+# print_colour(sara.description(), colours[5])
 
 lily = Student('Lily', emoji.emojize(
     "A technically perfect ballerina, :ballet_shoes:"),
     'facing intense pressure from her overbearing mother\n'
     'to be a principle ballerina, when,'
     'secretly dreaming of a career in singing.')
-print_colour(lily.description(), colours[6])
+# print_colour(lily.description(), colours[6])
+
+
+class Reactions:
+    """
+    Character reactions which the player chooses.
+    """
+
+    def __init__(self, positive, neutral, negative):
+        self.positive = positive
+        self.neutral = neutral
+        self.negative = negative
+
+    def reaction_positive(self):
+        return f'{self.positive}'
+
+    def reaction_neutral(self):
+        return f'{self.neutral}'
+
+    def reaction_negative(self):
+        return f'{self.negative}'
+
+
+# def choose_character():
+
+
+def game_play():
+    """
+    Main game play function
+    """
+    print_colour(georgia11_font.renderText("En Pointe"), colours[5])
+    print_colour(doom_font.renderText("Dance Academy Game"), colours[4])
+
+
+game_play()
